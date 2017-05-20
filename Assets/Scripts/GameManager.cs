e8a0f90a207931e99146dcb544ac8a7f1f64ae56
Jose Garcia <wizzarn@gmail.com>
2017-05-20 12:51:29 -0500

    init push

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	public IEnumerator ChangeScene(int SceneIndex){
+		float fadeTime = GameObject.Find ("_GM").GetComponent<Fading> ().BeginFade (1);
+		yield return new WaitForSeconds (fadeTime);
+		Application.LoadLevel (SceneIndex);
+	}
+	public void MyStatsScene(){
+		StartCoroutine(ChangeScene(1));
+	}
+	public void DungeonScene(){
+		StartCoroutine(ChangeScene(2));
+	}
+	public void MainScene(){
+		StartCoroutine(ChangeScene(0));
+	}
+}
